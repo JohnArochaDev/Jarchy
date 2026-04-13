17d1f80cf0826b42afd52ced86583e170d6d5d2e
John Arocha <jparocha777@gmail.com>
2026-04-12 17:21:19 -0700

    fix: updated package.json to include repository

---
+++ dist/index.mjs
@@ -0,0 +1,8 @@
+// src/components/Button/Button.tsx
+import { jsx } from "react/jsx-runtime";
+var Button = ({ children, onClick }) => {
+  return /* @__PURE__ */ jsx("button", { onClick, children });
+};
+export {
+  Button
+};
