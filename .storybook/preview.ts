cf3c113c8e8e38d4165b845ad337797c0f7b8f9a
John Arocha <jparocha777@gmail.com>
2026-04-14 15:17:21 -0700

    feat: added dynamic themes and constants

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,3 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import '../src/constants/colors.scss'
+
 import type { Preview } from '@storybook/react-vite'
 
 const preview: Preview = {
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,10 +3,11 @@ import styles from './Button.module.scss'
 export interface ButtonProps {
   label?: string
   onClick?: () => void
+  theme?: string
 }
 
 export const Button = (props: ButtonProps) => {
-  const { label = 'Button', onClick } = props
+  const { label = 'Button', onClick, theme = 'theme-black' } = props
 
-  return <button className={styles.button} onClick={onClick}>{label}</button>
+  return <button className={`${theme} ${styles.button}`} onClick={onClick}>{label}</button>
 }
